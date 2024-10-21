c64cb7eff94ae11ab9891955f06968db376a99fb
JamesSevil <jsclassified24@gmail.com>
2024-10-22 01:36:31 +0700

    zad2

---
+++ include/set.h
@@ -0,0 +1,31 @@
+#pragma once
+
+#include "includes.h"
+
+const int TABLE_SIZE = 100; // Размер хеш-таблицы
+
+template<typename T>
+struct NodeS {
+    T data;
+    NodeS* next;
+    NodeS(T value);
+};
+
+template<typename T>
+struct Set {
+    NodeS<T>* table[TABLE_SIZE];
+    int sizeset = 0;
+
+    Set();
+    ~Set();
+
+    int hashFunction(const T& value);
+    void insert(const T& value);
+    void remove(const T& value);
+    bool contains(const T& value);
+    T get(int index);
+    int size();
+    void print();
+};
+
+#include "../src/set.cpp"
