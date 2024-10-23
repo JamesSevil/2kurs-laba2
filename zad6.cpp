d91b9cd1d1c6c90adfd193db89aa71ff5b2ad50e
JamesSevil <jsclassified24@gmail.com>
2024-10-23 22:49:14 +0700

    update zad6

--- zad6.cpp
+++ zad6.cpp
@@ -1,41 +1,51 @@
 #include "include/hashtable.h"
 
-int findMaxSequence(string& str) {
+pair<int, string> findMaxSequence(string& str) {
     HashTable<int> charIndex;
-    int maxLenght = 0;
+    int maxLength = 0;
     int start = 0; // Начальный индекс текущей подстроки
+    string longestSubstr = ""; // Для хранения самой длинной подстроки
 
     for (int i = 0; i < str.length(); ++i) {
-        string token;
-        token += str[i];
+        string token(1, str[i]); // Создание строки из одного символа
         int value = charIndex.get(token);
         bool check = charIndex.find(token);
+        
         if (check && value >= start) { // Если символ уже встречался и его индекс больше или равен началу текущей подстроки
             start = value + 1;
         }
 
-        if (!check) charIndex.insert(token, i);
-        else charIndex.replace(token, i);
+        if (!check) {
+            charIndex.insert(token, i);
+        } else {
+            charIndex.replace(token, i);
+        }
 
-        maxLenght = max(maxLenght, i - start + 1);
+        int currentLength = i - start + 1;
+        if (currentLength > maxLength) {
+            maxLength = currentLength;
+            longestSubstr = str.substr(start, currentLength); // Обновляем самую длинную подстроку
+        }
     }
 
-    return maxLenght;
+    return {maxLength, longestSubstr};
 }
 
 int main() {
-
     while (true) {
         string str;
         cout << "Введите строку: ";
         getline(cin, str);
-        while (str.size() > 50.000) {
+        while (str.size() > 50000) {
             cout << "Строка не может содержать > 50.000 символов!" << endl;
             cout << "Повторите ввод: ";
             getline(cin, str);
         }
         if (str == "exit") break;
-        cout << "Дина самой длинной последовательности неповторяющихся символов: " << findMaxSequence(str) << endl;
+
+        auto result = findMaxSequence(str);
+        cout << "Длина самой длинной последовательности неповторяющихся символов: " << result.first << endl;
+        cout << "Самая длинная последовательность: " << result.second << endl;
     }
 
     return 0;
